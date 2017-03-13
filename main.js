ab4563e5bde2876feeae95cf3db68a302344b12e
Joe Bumbulis <joebumbulis@gmail.com>
2017-03-13 14:58:47 -0500

    halfway through adventurer mode

--- main.js
+++ main.js
@@ -1,17 +1,46 @@
 // setting up some variables to use for our examples. no need to mess with this:
-var numbers = [1,12,4,18,9,7,11,3,101,5,6];
-var strings = ['this','is','a','collection','of','words'];
-var instructors = [
-    { firstname : 'JD', teaches : 'JavaScript'},
-    { firstname : 'Tim', teaches : 'JavaScript'},
-    { firstname : 'Brit', teaches : 'Ruby'},
-    { firstname : 'Rhonda', teaches : 'iOS'},
-    { firstname : 'Gil', teaches : 'Java'},
-    { firstname : 'Ben', teaches : 'UI'},
-    { firstname : 'Jess', teaches : 'JavaScript'},
-    { firstname : 'Will', teaches : 'JavaScript'},
-    { firstname : 'Calvin', teaches : 'JavaScript'},
-    { firstname : 'James', teaches : 'Ruby'}
+var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
+var strings = ['this', 'is', 'a', 'collection', 'of', 'words'];
+var instructors = [{
+        firstname: 'JD',
+        teaches: 'JavaScript'
+    },
+    {
+        firstname: 'Tim',
+        teaches: 'JavaScript'
+    },
+    {
+        firstname: 'Brit',
+        teaches: 'Ruby'
+    },
+    {
+        firstname: 'Rhonda',
+        teaches: 'iOS'
+    },
+    {
+        firstname: 'Gil',
+        teaches: 'Java'
+    },
+    {
+        firstname: 'Ben',
+        teaches: 'UI'
+    },
+    {
+        firstname: 'Jess',
+        teaches: 'JavaScript'
+    },
+    {
+        firstname: 'Will',
+        teaches: 'JavaScript'
+    },
+    {
+        firstname: 'Calvin',
+        teaches: 'JavaScript'
+    },
+    {
+        firstname: 'James',
+        teaches: 'Ruby'
+    }
 ];
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -21,8 +50,8 @@ var instructors = [
 // Write a function called `sum` that takes two parameters and returns the sum
 // of those 2 numbers.
 // write your code here:
-function sum(num, num2){
-  return num + num2;
+function sum(num, num2) {
+    return num + num2;
 }
 
 console.log(sum);
@@ -30,17 +59,17 @@ console.log(sum);
 // write your console.log/asserts here:
 // i've already written some console.logs and console.asserts for you.
 // follow this pattern as you work on the rest of these problems.
-console.log('sum of 2 and 3 is ', sum(2,3));
-console.assert(sum(2,3), 5);
-console.assert(sum(1,0), 1);
-console.assert(sum(-48,5), 43);
+console.log('sum of 2 and 3 is ', sum(2, 3));
+console.assert(sum(2, 3), 5);
+console.assert(sum(1, 0), 1);
+console.assert(sum(-48, 5), 43);
 
 // -------------------
 // Write a function named `avg` that takes 3 parameters and returns the average
 // of those 3 numbers.
 // write your code here:
-function avg(num1, num2, num3){
-  return (num1 + num2 + num3)/3
+function avg(num1, num2, num3) {
+    return (num1 + num2 + num3) / 3
 }
 // write your console.log/asserts here:
 console.log(avg(40, 20, 60));
@@ -51,8 +80,8 @@ console.assert(avg(50, 'dog', 100), 40);
 // Write a function called `getLength` that takes one parameter (a string) and
 // returns the number of characters in that string
 // write your code here:
-function getLength(string){
-  return (string.length);
+function getLength(string) {
+    return (string.length);
 }
 // write your console.log/asserts here:
 console.log(getLength('Love is not against the law'));
@@ -63,12 +92,12 @@ console.log(getLength('math is hard'));
 // returns true if the second parameter is greater than the first. Otherwise
 // the function should return false.
 // write your code here:
-function greaterThan (first, second) {
-  if (second > first){
-    return true;
-  } else {
-    return false;
-  }
+function greaterThan(first, second) {
+    if (second > first) {
+        return true;
+    } else {
+        return false;
+    }
 }
 // write your console.log/asserts here:
 console.log(greaterThan(1, 9));
@@ -80,8 +109,8 @@ console.assert(greaterThan(1, 9), NaN);
 // string that is formatted like "Hello, Name!" where *Name* is the parameter
 // that was passed in.
 // write your code here:
-function greet(name){
-  return ('Hello ' + name + '!');
+function greet(name) {
+    return ('Hello ' + name + '!');
 };
 // write your console.log/asserts here:
 console.log(greet('Joe'));
@@ -93,8 +122,8 @@ console.assert(greet('Char'), 'Hello Char!');
 // The function should insert the words into a pre-defined sentence. Finally the
 // function should return that sentence.
 // write your code here:
-function madlib (w1, w2, w3, w4, w5, w6){
-  return ('Once there was a ' + w1 + ' who ' + w2 + " " + w3 + ' when it ' + w4 + ' for ' + w5 + ' in ' + w6 + '.');
+function madlib(w1, w2, w3, w4, w5, w6) {
+    return ('Once there was a ' + w1 + ' who ' + w2 + " " + w3 + ' when it ' + w4 + ' for ' + w5 + ' in ' + w6 + '.');
 }
 // write your console.log/asserts here:
 console.log(madlib('aardvark', 'ate', 'glitter', 'snowed', 'days', 'Japan'));
@@ -103,14 +132,14 @@ console.assert(madlib('lima', 'grapes', 'sun', 'rained', 'long time', 'USA'), 'O
 // -------------------
 // Write a function called `max` that returns the larger of two numbers
 // write your code here:
-function max(num1, num2){
-  if (num1 > num2){
-    return num1;
-  } else if (num2 > num1) {
-    return num2;
-  } else {
-    return ('choose two different #\'s')
-  }
+function max(num1, num2) {
+    if (num1 > num2) {
+        return num1;
+    } else if (num2 > num1) {
+        return num2;
+    } else {
+        return ('choose two different #\'s')
+    }
 }
 // write your console.log/asserts here:
 console.log(max(80, 90));
@@ -122,19 +151,55 @@ console.assert(max(100, 1), 100);
 // Write a function called `maxOfThree` that returns the largest of three
 // numbers
 // write your code here:
-function maxOfThree(num1, num2, num3){
-  if 
+function maxOfThree(num1, num2, num3) {
+    if (num1 > num2) {
+        if (num1 > num3) {
+            return (num1)
+        }
+    } if (num2 > num1) {
+        if (num2 > num3) {
+            return (num2)
+        }
+    } if (num3 > num1) {
+        if (num3 > num2) {
+            return (num3)
+        }
+    }
 }
-// write your console.log/asserts here:
 
+// // write your console.log/asserts here:
+console.log(maxOfThree(2, 4, 7));
+console.assert(maxOfThree(8, 2, 0), 8);
 
 // ---------------------
 // Write a function called `isVowel` that receives a single character as a
 // parameter and returns true if it is a vowel, false otherwise.
 // write your code here:
-
+function isVowel(v){
+  if (v === 'a'){
+    return (true)
+  }
+  if (v === 'e'){
+    return (true)
+  }
+  if (v === 'i'){
+    return (true)
+  }
+  if (v === 'o'){
+    return (true)
+  }
+  if (v === 'u'){
+    return (true)
+  }
+  else {
+    return (false)
+  }
+}
 // write your console.log/asserts here:
-
+console.log(isVowel('o'));
+console.assert(isVowel('u'), true);
+console.assert(isVowel('a'), true);
+console.assert(isVowel('e'), true);
 
 // ---------------------
 // Write a function called `translate` that will translate a text into
@@ -226,7 +291,7 @@ console.assert(largest(numbers), 101);
 // write your code here:
 
 // write your console.log/asserts here:
-console.log('the objects that match javascript teachers are', objectMatches('JavaScript', 'teaches', instructors))
+// console.log('the objects that match javascript teachers are', objectMatches('JavaScript', 'teaches', instructors))
 
 ///////////////////////////////////////////////////////////////////////////////
 //                                                      Epic Mode                                                                            //
@@ -248,10 +313,10 @@ console.log('the objects that match javascript teachers are', objectMatches('Jav
 // Write a function called `isValidPassword` that takes a string password. If
 // the password meets the following criteria return true. otherwise return
 // false.
-    // at least 8 characters
-    // contains at least one number
-    // contains at least one of the following: . , ! $ &
-    // contains at least one capital letter and one lowercase letter
+// at least 8 characters
+// contains at least one number
+// contains at least one of the following: . , ! $ &
+// contains at least one capital letter and one lowercase letter
 // write your code here:
 
 // write your console.log/asserts here:
